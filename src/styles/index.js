7c09dd62a1afe2b860be834703f3c228e38f124a
Cody Taft <cody.taft@gmail.com>
2021-02-17 18:55:46 -0700

    Remove filler

---
+++ src/styles/index.js
@@ -0,0 +1,57 @@
+// styles
+export const pageStyles = {
+  color: '#232129',
+  padding: '96px',
+  fontFamily: '-apple-system, Roboto, sans-serif, serif',
+};
+export const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+};
+export const headingAccentStyles = {
+  color: '#663399',
+};
+export const paragraphStyles = {
+  marginBottom: 48,
+};
+export const codeStyles = {
+  color: '#8A6534',
+  padding: 4,
+  backgroundColor: '#FFF4DB',
+  fontSize: '1.25rem',
+  borderRadius: 4,
+};
+export const listStyles = {
+  marginBottom: 96,
+  paddingLeft: 0,
+};
+export const listItemStyles = {
+  fontWeight: '300',
+  fontSize: '24px',
+  maxWidth: '560px',
+};
+
+export const linkStyle = {
+  color: '#8954A8',
+  fontWeight: 'bold',
+  fontSize: '16px',
+  verticalAlign: '5%',
+};
+
+export const docLinkStyle = {
+  ...linkStyle,
+  listStyleType: 'none',
+  marginBottom: 24,
+};
+
+export const descriptionStyle = {
+  color: '#232129',
+  fontSize: '14px',
+};
+
+export const docLink = {
+  text: 'Documentation',
+  url: 'https://www.gatsbyjs.com/docs/',
+  color: '#8954A8',
+};
